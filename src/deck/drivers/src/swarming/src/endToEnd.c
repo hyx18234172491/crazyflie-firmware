73e09b108bf18424799a4b9c2e5864a08de663cb
HuoHaodong <inf1nityhhd@163.com>
2021-04-03 15:56:46 +0800

    packet loss test

--- src/deck/drivers/src/swarming/src/endToEnd.c
+++ src/deck/drivers/src/swarming/src/endToEnd.c
@@ -3,59 +3,60 @@
 #include "olsrDebug.h"
 #include "uwbOlsr.h"
 #include "log.h"
-#define SENDER_ADDRESS 28
-#define RECEIVER_ADDRESS 29
-#define TOTAL_PACKET_NUM 1000
+#define SENDER_ADDRESS 1
+#define RECEIVER_ADDRESS 2
+#define TOTAL_PACKET_NUM 10000
 bool isSender = false;
 bool isReceiver = false;
 float receiveRate = 0.0;
-
-void endToEndTask()
-{
+static uint16_t sendCount = 0;
+static uint16_t recvCount = 0;
+void endToEndTask() {
   adHocAppInit(ADHOC_PORT_END_TO_END_DELAY);
-  int sendCount = 0;
-  int recvCount = 0;
-  if(myAddress==RECEIVER_ADDRESS) {
+
+  if (myAddress == RECEIVER_ADDRESS) {
     isReceiver = true;
-  }else if(myAddress==SENDER_ADDRESS) {
+  } else if (myAddress == SENDER_ADDRESS) {
     isSender = true;
   }
-  if(isReceiver)
-    {
-      DEBUG_PRINT_OLSR_APP("Im Receiver!\n");
-    }
-  if(isSender)
-    {
-      DEBUG_PRINT_OLSR_APP("Im Sender!\n");
-    }
-  if(!isReceiver&&!isSender)
-    {
-      DEBUG_PRINT_OLSR_APP("Im Route!\n");
-    }
+  if (isReceiver) {
+    DEBUG_PRINT_OLSR_APP("Im Receiver!\n");
+  }
+  if (isSender) {
+    DEBUG_PRINT_OLSR_APP("Im Sender!\n");
+  }
+  if (!isReceiver && !isSender) {
+    DEBUG_PRINT_OLSR_APP("Im Route!\n");
+  }
   int seq = 1;
   int firstSeq = 0;
-  while(1)
-    {
-      if(isSender&&sendCount<TOTAL_PACKET_NUM)
-        {
-          olsrSendData(myAddress,ADHOC_PORT_END_TO_END_DELAY,RECEIVER_ADDRESS,ADHOC_PORT_END_TO_END_DELAY,seq++,NULL,0);
-          sendCount++;
-        }
-      if(isReceiver)
-        {
-          olsrDataMessage_t msg;
-          adHocGetfromQueue(ADHOC_PORT_END_TO_END_DELAY,&msg);
-          recvCount++;
-          if (!firstSeq)
-            firstSeq = msg.m_dataHeader.m_seq;
-          receiveRate = recvCount * 100.0 / (msg.m_dataHeader.m_seq - firstSeq + 1);
-          DEBUG_PRINT_OLSR_APP("Recv: %d; Seq: %d; Rate: %.2f.\n", recvCount, msg.m_dataHeader.m_seq, receiveRate);
-        }
-        // DEBUG_PRINT_OLSR_APP("recv pack:%d\n",recvCount);
-        vTaskDelay(500);
+  while (1) {
+    if (isSender && sendCount < TOTAL_PACKET_NUM) {
+      olsrSendData(myAddress,
+                   ADHOC_PORT_END_TO_END_DELAY,
+                   RECEIVER_ADDRESS,
+                   ADHOC_PORT_END_TO_END_DELAY,
+                   seq++,
+                   NULL,
+                   0);
+      sendCount++;
     }
-
-}   
+    if (isReceiver) {
+      olsrDataMessage_t msg;
+      adHocGetfromQueue(ADHOC_PORT_END_TO_END_DELAY, &msg);
+      recvCount++;
+      if (!firstSeq) {
+        firstSeq = msg.m_dataHeader.m_seq;
+      }
+      receiveRate = recvCount * 100.0 / (msg.m_dataHeader.m_seq - firstSeq + 1);
+      DEBUG_PRINT_OLSR_APP("Recv: %d; Seq: %d; Rate: %.2f.\n", recvCount, msg.m_dataHeader.m_seq, receiveRate);
+    }
+    // DEBUG_PRINT_OLSR_APP("recv pack:%d\n",recvCount);
+    vTaskDelay(50);
+  }
+}
 LOG_GROUP_START(ReceiveRate)
-  LOG_ADD(LOG_FLOAT, receiveRate, &receiveRate)
+        LOG_ADD(LOG_FLOAT, receiveRate, &receiveRate)
+        LOG_ADD(LOG_UINT16, receiveCount, &recvCount)
+        LOG_ADD(LOG_UINT16, sendCount, &sendCount)
 LOG_GROUP_STOP(RECEIVE_RATE)
--- src/deck/drivers/src/swarming/src/uwbOlsr.c
+++ src/deck/drivers/src/swarming/src/uwbOlsr.c
@@ -17,129 +17,99 @@
 #include "endToEnd.h"
 #include "adHocOnBoardSim.h"
 
-
-static void olsrStructInit(dwDevice_t *dev)
-{
+static void olsrStructInit(dwDevice_t *dev) {
   DEBUG_PRINT_OLSR_SYSTEM("START OLSR_STRUCT_INIT\n");
   olsrStructInitAll(dev);
   olsrDeviceInit(dev);
 }
-static void olsrHelloTaskInit()
-{
+static void olsrHelloTaskInit() {
   DEBUG_PRINT_OLSR_SYSTEM("START_OLSR_HELLO_TASK_INIT\n");
-  if(xTaskCreate(olsrHelloTask, "OLSR_HELLO", 3*configMINIMAL_STACK_SIZE, NULL,LPS_DECK_TASK_PRI, NULL)==pdPASS)
-    {
-      DEBUG_PRINT_OLSR_SYSTEM("HELLO TASK CREATE SUCCESSFUL\n");
-    }
-  else
-    {
-      DEBUG_PRINT_OLSR_SYSTEM("HELLO TASK CREATE FAILD\n");
-    }
+  if (xTaskCreate(olsrHelloTask, "OLSR_HELLO", 3 * configMINIMAL_STACK_SIZE, NULL, LPS_DECK_TASK_PRI, NULL) == pdPASS) {
+    DEBUG_PRINT_OLSR_SYSTEM("HELLO TASK CREATE SUCCESSFUL\n");
+  } else {
+    DEBUG_PRINT_OLSR_SYSTEM("HELLO TASK CREATE FAILD\n");
+  }
 }
 static void olsrTcTaskInit() //TODO:修改stacksize
 {
   DEBUG_PRINT_OLSR_SYSTEM("START_OLSR_TC_TASK_INIT\n");
-  if(xTaskCreate(olsrTcTask, "OLSR_TC", configMINIMAL_STACK_SIZE, NULL,LPS_DECK_TASK_PRI, NULL)==pdPASS)
-    {
-      DEBUG_PRINT_OLSR_SYSTEM("TC TASK CREATE SUCCESSFUL\n");
-    }
-  else
-    {
-      DEBUG_PRINT_OLSR_SYSTEM("TC TASK CREATE FAILD\n");
-    }
+  if (xTaskCreate(olsrTcTask, "OLSR_TC", configMINIMAL_STACK_SIZE, NULL, LPS_DECK_TASK_PRI, NULL) == pdPASS) {
+    DEBUG_PRINT_OLSR_SYSTEM("TC TASK CREATE SUCCESSFUL\n");
+  } else {
+    DEBUG_PRINT_OLSR_SYSTEM("TC TASK CREATE FAILD\n");
+  }
 }
-static void olsrTsTaskInit()
-{
+static void olsrTsTaskInit() {
   DEBUG_PRINT_OLSR_SYSTEM("START_OLSR_TS_TASK_INIT\n");
-  if(xTaskCreate(olsrTsTask, "OLSR_TS", configMINIMAL_STACK_SIZE, NULL,LPS_DECK_TASK_PRI, NULL)==pdPASS)
-    {
-      DEBUG_PRINT_OLSR_SYSTEM("TS TASK CREATE SUCCESSFUL\n");
-    }
-  else
-    {
-      DEBUG_PRINT_OLSR_SYSTEM("TS TASK CREATE FAILD\n");
-    }
+  if (xTaskCreate(olsrTsTask, "OLSR_TS", configMINIMAL_STACK_SIZE, NULL, LPS_DECK_TASK_PRI, NULL) == pdPASS) {
+    DEBUG_PRINT_OLSR_SYSTEM("TS TASK CREATE SUCCESSFUL\n");
+  } else {
+    DEBUG_PRINT_OLSR_SYSTEM("TS TASK CREATE FAILD\n");
+  }
 }
-static void olsrSendTaskInit(dwDevice_t *dev)
-{
+static void olsrSendTaskInit(dwDevice_t *dev) {
   DEBUG_PRINT_OLSR_SYSTEM("START_OLSR_SEND_TASK_INIT\n");
-  if(xTaskCreate(olsrSendTask, "OLSR_SEND", 2*configMINIMAL_STACK_SIZE, dev,LPS_DECK_TASK_PRI, NULL)==pdPASS)
-    {
-      DEBUG_PRINT_OLSR_SYSTEM("SEND TASK CREATE SUCCESSFUL\n");
-    }
-  else
-    {
-      DEBUG_PRINT_OLSR_SYSTEM("SEND TASK CREATE FAILD\n");
-    }
+  if (xTaskCreate(olsrSendTask, "OLSR_SEND", 2 * configMINIMAL_STACK_SIZE, dev, LPS_DECK_TASK_PRI, NULL) == pdPASS) {
+    DEBUG_PRINT_OLSR_SYSTEM("SEND TASK CREATE SUCCESSFUL\n");
+  } else {
+    DEBUG_PRINT_OLSR_SYSTEM("SEND TASK CREATE FAILD\n");
+  }
 }
-static void olsrRecvTaskInit(dwDevice_t *dev)
-{
+static void olsrRecvTaskInit(dwDevice_t *dev) {
   DEBUG_PRINT_OLSR_SYSTEM("START_OLSR_RECV_TASK_INIT\n");
-  if(xTaskCreate(olsrRecvTask, "OLSR_RECV", 4*configMINIMAL_STACK_SIZE, dev,4, NULL)==pdPASS)
-    {
-      DEBUG_PRINT_OLSR_SYSTEM("RECV TASK CREATE SUCCESSFUL\n");
-    }
-  else
-    {
-      DEBUG_PRINT_OLSR_SYSTEM("RECV TASK CREATE FAILD\n");
-    }
+  if (xTaskCreate(olsrRecvTask, "OLSR_RECV", 4 * configMINIMAL_STACK_SIZE, dev, 4, NULL) == pdPASS) {
+    DEBUG_PRINT_OLSR_SYSTEM("RECV TASK CREATE SUCCESSFUL\n");
+  } else {
+    DEBUG_PRINT_OLSR_SYSTEM("RECV TASK CREATE FAILD\n");
+  }
 }
-static void olsrPacketLossTaskInit()
-{
-  if(xTaskCreate(olsrPacketLossTask, "PacketLoss", configMINIMAL_STACK_SIZE, NULL ,LPS_DECK_TASK_PRI, NULL)==pdPASS)
-    {
-      DEBUG_PRINT_OLSR_SYSTEM("OLSR_APP TASK CREATE SUCCESSFUL\n");
-    }
-  else
-    {
-      DEBUG_PRINT_OLSR_SYSTEM("OLSR_APP TASK CREATE FAILD\n");
-    } 
+static void olsrPacketLossTaskInit() {
+  if (xTaskCreate(olsrPacketLossTask, "PacketLoss", configMINIMAL_STACK_SIZE, NULL, LPS_DECK_TASK_PRI, NULL)
+      == pdPASS) {
+    DEBUG_PRINT_OLSR_SYSTEM("OLSR_APP TASK CREATE SUCCESSFUL\n");
+  } else {
+    DEBUG_PRINT_OLSR_SYSTEM("OLSR_APP TASK CREATE FAILD\n");
+  }
 }
-static void olsrAppTaskInit()
-{
+static void olsrAppTaskInit() {
   DEBUG_PRINT_OLSR_SYSTEM("START_OLSR_APP_TASK_INIT\n");
-  if(xTaskCreate(endToEndTask, "OLSR_APP", 2*configMINIMAL_STACK_SIZE, NULL ,LPS_DECK_TASK_PRI, NULL)==pdPASS)
-    {
-      DEBUG_PRINT_OLSR_SYSTEM("OLSR_APP TASK CREATE SUCCESSFUL\n");
-    }
-  else
-    {
-      DEBUG_PRINT_OLSR_SYSTEM("OLSR_APP TASK CREATE FAILD\n");
-    } 
+  if (xTaskCreate(endToEndTask, "OLSR_APP", 2 * configMINIMAL_STACK_SIZE, NULL, LPS_DECK_TASK_PRI, NULL) == pdPASS) {
+    DEBUG_PRINT_OLSR_SYSTEM("OLSR_APP TASK CREATE SUCCESSFUL\n");
+  } else {
+    DEBUG_PRINT_OLSR_SYSTEM("OLSR_APP TASK CREATE FAILD\n");
+  }
 }
 
-static void olsrTaskInit(dwDevice_t *dev)
-{
-    DEBUG_PRINT_OLSR_SYSTEM("TASK_INIT");
-    // olsrPacketLossTaskInit();
-    olsrHelloTaskInit();
-    olsrTcTaskInit();
-    olsrTsTaskInit();
-    olsrSendTaskInit(dev);
-    olsrRecvTaskInit(dev);
-    olsrAppTaskInit();
-    // initSimTopology();
+static void olsrTaskInit(dwDevice_t *dev) {
+  DEBUG_PRINT_OLSR_SYSTEM("TASK_INIT");
+  // olsrPacketLossTaskInit();
+  olsrHelloTaskInit();
+  olsrTcTaskInit();
+//    olsrTsTaskInit();
+  olsrSendTaskInit(dev);
+  olsrRecvTaskInit(dev);
+  olsrAppTaskInit();
+  // initSimTopology();
+
 }
 
-static void olsrInit(dwDevice_t *dev) 
-{
-    systemWaitStart();
-    DEBUG_PRINT_OLSR_SYSTEM("init to new deck OLSR");
-    int myChanel = configblockGetRadioChannel();
-    myAddress = myChanel|0x0000;
-    olsrStructInit(dev);
-    olsrTaskInit(dev);
+static void olsrInit(dwDevice_t *dev) {
+  systemWaitStart();
+  DEBUG_PRINT_OLSR_SYSTEM("init to new deck OLSR");
+  int myChanel = configblockGetRadioChannel();
+  myAddress = myChanel | 0x0000;
+  olsrStructInit(dev);
+  olsrTaskInit(dev);
 }
 
-static bool isRangingOk()
-{
+static bool isRangingOk() {
   return true;
 }
 
 point_t anchorPosition[2];
-static bool getAnchorPosition(const uint8_t anchorId, point_t* position) {
-    *position = anchorPosition[anchorId];
-    return true;
+static bool getAnchorPosition(const uint8_t anchorId, point_t *position) {
+  *position = anchorPosition[anchorId];
+  return true;
 }
 
 static uint8_t getAnchorIdList(uint8_t unorderedAnchorList[], const int maxListSize) {
@@ -151,11 +121,11 @@ static uint8_t getActiveAnchorIdList(uint8_t unorderedAnchorList[], const int ma
 }
 
 uwbAlgorithm_t uwbOLSRAlgorithm = {
-  .init = olsrInit,
-  .isRangingOk = isRangingOk,
-  .getAnchorPosition = getAnchorPosition,
-  .getAnchorIdList = getAnchorIdList,
-  .getActiveAnchorIdList = getActiveAnchorIdList,
+    .init = olsrInit,
+    .isRangingOk = isRangingOk,
+    .getAnchorPosition = getAnchorPosition,
+    .getAnchorIdList = getAnchorIdList,
+    .getActiveAnchorIdList = getActiveAnchorIdList,
 };
 
 
