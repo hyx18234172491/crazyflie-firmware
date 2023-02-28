08e1a8a8c36171d261d554c791ada7cd64638e34
hyx18234172491 <hyx18234172491@163.com>
2023-02-28 21:40:40 +0800

    feat:完成了相对位置估计中通信部分

--- src/deck/drivers/interface/swarm_ranging.h
+++ src/deck/drivers/interface/swarm_ranging.h
@@ -11,10 +11,28 @@
 #define RANGING_RX_QUEUE_ITEM_SIZE sizeof(Ranging_Message_With_Timestamp_t)
 
 /* Ranging Constants */
-#define RANGING_INTERVAL_MIN 20 // default 20
+#define RANGING_INTERVAL_MIN 20  // default 20
 #define RANGING_INTERVAL_MAX 500 // default 500
 #define Tf_BUFFER_POOL_SIZE (4 * RANGING_INTERVAL_MAX / RANGING_INTERVAL_MIN)
 #define TX_PERIOD_IN_MS 100
+/*---自己添加---start---*/
+typedef struct
+{
+    uint16_t distanceTowards[RANGING_TABLE_SIZE + 1]; // cm
+    short velocityXInWorld[RANGING_TABLE_SIZE + 1];   // 2byte m/s 在世界坐标系下的速度（不是机体坐标系）
+    short velocityYInWorld[RANGING_TABLE_SIZE + 1];   // 2byte cm/s 在世界坐标系下的速度（不是机体坐标系）
+    float gyroZ[RANGING_TABLE_SIZE + 1];
+    uint16_t positionZ[RANGING_TABLE_SIZE + 1]; // cm
+    bool refresh[RANGING_TABLE_SIZE + 1];
+    bool isNewAdd[RANGING_TABLE_SIZE + 1]; // 这个邻居是否是新加入的
+} neighborStateInfo_t;
+
+typedef struct
+{
+    address_t address[RANGING_TABLE_SIZE + 1];
+    int size;
+} currentNeighborAddressInfo_t; /*当前正在和本无人机进行通信的邻居地址信息*/
+/*---自己添加---end---*/
 
 /* Ranging Operations */
 void rangingInit();
@@ -25,5 +43,11 @@ void processRangingMessage(Ranging_Message_With_Timestamp_t *rangingMessageWithT
 int generateRangingMessage(Ranging_Message_t *rangingMessage);
 int16_t getDistance(uint16_t neighborAddress);
 void setDistance(uint16_t neighborAddress, int16_t distance);
+/*---自己添加---start---*/
+void setNeighborStateInfo(uint16_t neighborAddress, int16_t distance, Ranging_Message_Header_t *rangingMessageHeader, bool isNewAddNeighbor);
+bool getNeighborStateInfo(uint16_t neighborAddress, uint16_t *distance, short *vx, short *vy, float *gyroZ, uint16_t *height, bool *isNewAddNeighbor);
+bool getOrSetKeepflying(int RobIDfromControl, bool keep_flying);
+void getCurrentNeighborAddressInfo_t(currentNeighborAddressInfo_t *currentNeighborAddressInfo); /*供外部调用，获取当前无人机的所有正在通信的邻居的地址*/
+/*---自己添加---end---*/
 
 #endif
