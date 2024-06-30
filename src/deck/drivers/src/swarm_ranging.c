e4c0383e326231e5ab66f5e7f9cd1fe2e2487759
hyx <18234172491@163.com>
2024-06-30 13:25:46 +0800

    fix bug

--- src/deck/drivers/src/swarm_ranging.c
+++ src/deck/drivers/src/swarm_ranging.c
@@ -1426,7 +1426,7 @@ static void processRangingMessage(Ranging_Message_With_Timestamp_t *rangingMessa
   }
   Timestamp_Tuple_t Tf = findTfBySeqNumber(neighborRf.seqNumber);
 
-  if (neighborRf.seqNumber != neighborRangingTable->Tp.seqNumber)
+  if (neighborRf.seqNumber != neighborRangingTable->Tp.seqNumber && Tf.timestamp.full)
   {
     neighborRangingTable->Rf = neighborRf;
     rangingTableOnEvent(neighborRangingTable, RANGING_EVENT_RX_Rf);
